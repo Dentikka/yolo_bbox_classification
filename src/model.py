e4e488b50a840410fe2dfe9fe0513c6578d3a5f6
Dentikka <shveikin.da@phystech.edu>
2024-03-11 16:13:21 +0300

    model

---
+++ src/model.py
@@ -0,0 +1,49 @@
+import torch
+from torch import nn, Tensor
+
+import timm
+
+
+class BboxClassificationModel(nn.Module):
+    """
+    A class to make a model consisting of an embedding model (backbone)
+    and classifier
+    Currently maintained architectures are:
+        MobileNet, EfficientNet, ConvNext, ResNet, ViT
+    """
+    def __init__(self,
+                 cfg_model: dict, 
+                 n_classes: int):
+        super().__init__()
+        self.emb_model = timm.create_model(cfg_model['model'], pretrained=cfg_model['pretrained'])
+        self.emb_size = self.emb_model.num_features
+        self.emb_model.reset_classifier(0)  # a simpler way to get emb_model from a timm model
+
+        self.classifier = nn.Sequential(
+            nn.Linear(self.emb_size, n_classes)
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        emb = self.emb_model(x)
+        return self.classifier(emb)
+
+    def set_backbone_state(self, state: str) -> None:
+        assert state in ['freeze', 'unfreeze']
+        for param in self.emb_model.parameters():
+            if state == 'freeze':
+                param.requires_grad = False
+            elif state == 'unfreeze':
+                param.requires_grad = True
+    
+
+def get_model(cfg_model: dict,
+              n_classes: int,
+              device: torch.device='cpu',
+              compile: bool=False) -> BboxClassificationModel:
+    model = BboxClassificationModel(cfg_model, n_classes)
+
+    model.to(device)
+    if compile:
+        model = torch.compile(model, dynamic=True)
+
+    return model
